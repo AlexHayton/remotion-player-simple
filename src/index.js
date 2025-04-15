664f1dc033b510400e16deb5a6e80cebbe906225
Alex Hayton <ahayton@adobe.com>
2025-04-15 14:46:43 +0100

    React 19

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { createRoot } from "react-dom/client";
 import * as React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
@@ -6,12 +7,11 @@ import reportWebVitals from "./reportWebVitals";
 
 console.log("ReactDOM", ReactDOM);
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const root = createRoot(document.getElementById("root"));
+
+root.render(<React.StrictMode>
+  <App />
+</React.StrictMode>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
