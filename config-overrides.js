a78f04c7cf41853be32d2f008b634ad7eff34093
Alex Hayton <ahayton@adobe.com>
2025-04-25 13:06:43 +0100

    Upgrade to react 18

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,4 @@
 module.exports = function override(config, env) {
-  config.resolve.alias = {
-    "react/jsx-dev-runtime": "react/jsx-dev-runtime.js",
-    "react/jsx-runtime": "react/jsx-runtime.js",
-  };
-
   // New config, e.g. config.plugins.push...
   return config;
 };
